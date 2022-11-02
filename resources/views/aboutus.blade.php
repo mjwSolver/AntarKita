5b3763f643487f2399c673df22a247623c36d50c
mysteryid <62708654+mysteryid@users.noreply.github.com>
2022-11-02 14:13:40 +0700

    Update aboutus.blade.php

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -4,6 +4,21 @@
 
 @section('container')
 
+<section class="p-12 h-[50vh] text-center relative bg-no-repeat bg-cover rounded-lg pt-32"
+		{{-- style="background-image: url('images/singles/single3_sm.jpg'); height: 500px;"> --}}
+		style="background-image: url('images/prewed/prewed6_sm.jpg');">
+		<div class="absolute inset-0 w-full h-full overflow-hidden bg-fixed" style="background-color: rgba(0, 0, 0, 0.6)">
+			<div class="flex justify-center items-center h-full">
+				<div class="text-white">
+					<h2 class="font-semibold text-4xl mb-4 mt-10">Portfolio</h2>
+					<span class="font-normal text-lg mb-6">Berikut adalah portfolio AntarKita</span>
+				</div>
+			</div>
+		</div>
+</section>
+
+
+
 <div
                     class="px-4 sm:px-6 sm:text-center md:mx-auto md:max-w-2xl lg:col-span-6 lg:flex lg:items-center lg:text-left">
                     <div>
