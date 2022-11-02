e9cc1fb076f5f372b40bd3b430c2ae728e9ea0c1
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2022-11-02 22:21:28 +0700

    Portfolio with MVC
    Added Routing
    Created Controller
    Created Model
    Configured View with forEach

---
+++ app/Models/Photo.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    use HasFactory;
+
+    public static $photos=[
+        // Green Theme
+        [
+            'title' => "My darling",
+            'sub' => "Prewed",
+            'imagepath' => "prewed/prewed2_sm.jpg"
+        ],
+        [
+            'title' => "We Face",
+            'sub' => "Prewed",
+            'imagepath' => "prewed/prewed1_sm.jpg"
+        ],
+        [
+            'title' => "Just Us",
+            'sub' => "Prewed",
+            'imagepath' => "prewed/prewed3_sm.jpg"
+        ],
+        // Black theme
+        [
+            'title' => "Just Black",
+            'sub' => "Prewed",
+            'imagepath' => "prewed/prewed4_sm.jpg"
+        ],
+        [
+            'title' => "Just Us",
+            'sub' => "Prewed",
+            'imagepath' => "prewed/prewed5_sm.jpg"
+        ],
+        [
+            'title' => "It's Bright",
+            'sub' => "Prewed",
+            'imagepath' => "prewed/prewed6_sm.jpg"
+        ],
+        // Singles
+        [
+            'title' => "What?",
+            'sub' => "Portrait",
+            'imagepath' => "singles/single1_sm.jpg"
+        ],
+        [
+            'title' => "I think so",
+            'sub' => "Portrait",
+            'imagepath' => "singles/single2_sm.jpg"
+        ],
+        [
+            'title' => "Attention",
+            'sub' => "Portrait",
+            'imagepath' => "singles/single3_sm.jpg"
+        ],
+        [
+            'title' => "Overglow",
+            'sub' => "Portrait",
+            'imagepath' => "singles/single4_sm.jpg"
+        ],
+    ];
+
+    public static function index() {
+        return collect(self::$photos);
+    }
+}
+
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,10 +4,6 @@
 
 @section('container')
 
-{{-- @foreach ($collection as $item)
-	
-@endforeach --}}
-
 <section class="p-12 h-[50vh] text-center relative bg-no-repeat bg-cover rounded-lg pt-32"
 		{{-- style="background-image: url('images/singles/single3_sm.jpg'); height: 500px;"> --}}
 		style="background-image: url('images/prewed/prewed6_sm.jpg');">
@@ -21,49 +17,22 @@
 		</div>
 </section>
 
-<section class="gap-8 space-y-8 columns-2 md:columns-3 lg:columns-4 p-8 pt-32">
+<section class="gap-8 space-y-8 columns-2 md:columns-4 lg:columns-4 xl:columns-5 p-8 pt-20 md:pt-28 lg:pt-30 xl:pt-32">
+
+	@foreach ($photos as $photo)
+
 	<figure class="relative max-w-xs sm:max-w-sm md:max-w-md cursor-pointer">
 		<img class="rounded-lg shadow-xl hover:shadow-2xl"
-			src="/images/prewed/prewed2_sm.jpg" alt="prewed">
-		<figcaption class="absolute text-lg -mt-16 text-white w-full px-4 py-1 bg-zinc-800/50 rounded-lg backdrop-blur-sm">
+			src="/images/{{$photo['imagepath']}}" alt="prewed">
+		<figcaption class="static text-lg -mt-16 text-white w-full px-4 py-1 bg-zinc-800/50 rounded-lg backdrop-blur-sm">
 		  <div class="font-sans font-medium">
-			<h1>Portrait</h1>
+			<h1>{{$photo['title']}}</h1>
 		  </div>
-		  <div> <span class="font-sans font-light">Sunny outside</span> </div>
+		  <div> <span class="font-sans font-light">{{$photo['sub']}}</span> </div>
 		  </figcaption>
 	  </figure>
 
-	  <figure class="relative max-w-xs sm:max-w-sm md:max-w-md cursor-pointer">
-		<img class="rounded-lg shadow-xl hover:shadow-2xl"
-			src="/images/prewed/prewed2_sm.jpg" alt="prewed">
-		<figcaption class="absolute text-lg -mt-16 text-white w-full px-4 py-1 bg-zinc-800/50 rounded-lg backdrop-blur-sm">
-		  <div class="font-sans font-medium">
-			<h1>Portrait</h1>
-		  </div>
-		  <div> <span class="font-sans font-light">Sunny outside</span> </div>
-		  </figcaption>
-	  </figure>
+	@endforeach
 
-	  <figure class="relative max-w-xs sm:max-w-sm md:max-w-md cursor-pointer">
-		<img class="rounded-lg shadow-xl hover:shadow-2xl"
-			src="/images/prewed/prewed1_sm.jpg" alt="prewed">
-		<figcaption class="absolute text-lg -mt-16 text-white w-full px-4 py-1 bg-zinc-800/50 rounded-lg backdrop-blur-sm">
-		  <div class="font-sans font-medium">
-			<h1>Portrait</h1>
-		  </div>
-		  <div> <span class="font-sans font-light">Sunny outside</span> </div>
-		  </figcaption>
-	  </figure>
-
-	  <figure class="relative max-w-xs sm:max-w-sm md:max-w-md cursor-pointer">
-		<img class="rounded-lg shadow-xl hover:shadow-2xl"
-			src="/images/prewed/prewed1_sm.jpg" alt="prewed">
-		<figcaption class="absolute text-lg -mt-16 text-white w-full px-4 py-1 bg-zinc-800/50 rounded-lg backdrop-blur-sm">
-		  <div class="font-sans font-medium">
-			<h1>Portrait</h1>
-		  </div>
-		  <div> <span class="font-sans font-light">Sunny outside</span> </div>
-		  </figcaption>
-	  </figure>
 </section>
 @endsection
